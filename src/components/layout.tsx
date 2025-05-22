16f97f8ca9411f3a8a0e9d771e6bebdb3ff78091
SHIN09081 <ohjumo57@gmail.com>
2025-05-22 16:23:27 +0900

    12주차

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,80 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { auth } from "../routes/firebase";
+
+const Wrapper = styled.div`
+    display: grid;
+    gap: 20px;
+    grid-template-columns: 1fr 4fr;
+    height: 100%;
+    padding: 50px;
+    width 100%;
+    max-width: 860px;
+`;
+
+const Menu = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+`;
+
+const MenuItem = styled.div`
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid white;
+    height: 50px;
+    width: 50px;
+    border-radius: 25px;
+    svg {
+        width: 30px;
+        fill: white;
+    }
+
+    &.log-out {
+        border-color: tomato;
+        svg{
+            fill: tomato;
+        }
+    }
+`;
 
 export default function Layout() {
+    const navigate = useNavigate();
+    const onLogOut = async() => {
+        const ok = confirm("Are you sure you want to log out?");
+        if(ok){
+            await auth.signOut();
+            navigate("/login");
+        }
+
+    };
     return (
-        <>
+        <Wrapper>
+            <Menu>
+                <Link to="/">
+                <MenuItem>
+                <svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
+</svg>
+                </MenuItem>
+                </Link>
+                <Link to="/profile">
+                <MenuItem>
+                <svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"></path>
+</svg>          
+                </MenuItem>
+                </Link>
+                <MenuItem onClick={onLogOut} className="log-out">
+                <svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6 9 12.75l4.286-4.286a11.948 11.948 0 0 1 4.306 6.43l.776 2.898m0 0 3.182-5.511m-3.182 5.51-5.511-3.181"></path>
+</svg>
+                </MenuItem>
+            </Menu>
             <Outlet />
-        </>
+        </Wrapper>
     );
 }
